284be39e62b2b88bfd69b7fd346939c85eaafd75
Ben Munge <bmunge@CHIC02QV13BG8WN.sea.corp.expecn.com>
2017-01-30 11:45:24 -0600

    fix history

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function displayHistory(response, tokens) {
 	}
 
 	db.task(t=> {
-		var query = 'select * from game_log where upper(winner) = $1 && upper(loser) = $2';
+		var query = 'select * from game_log where upper(winner) = $1 and upper(loser) = $2';
 		return t.batch([
 			t.any(query, player1.toUpperCase(), player2.toUpperCase()),
 			t.any(query, player2.toUpperCase(), player1.toUpperCase())
