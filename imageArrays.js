a021f73957c7f610095c15e8ef632aa92bdf6e8e
Ben Munge <bmunge@CHIC02QV13BG8WN.sea.corp.expecn.com>
2017-05-23 14:58:05 -0500

    Fix

--- imageArrays.js
+++ imageArrays.js
@@ -1,5 +1,16 @@
 imageArrays.exports = {
-	var rajArray = [
+  rajArray: var {
+    rajArray
+  },
+  kevinGifArray: var {
+    kevinGifArray
+  },
+  kevinPictureArray: var {
+  	kevinPictureArray
+  }
+};
+
+var rajArray = [
 	'https://media.giphy.com/media/3o6gaQfzKVaQi43l3a/giphy.gif',
 	'https://media.giphy.com/media/26FPzCq1HT3PYQrzq/giphy.gif',
 	'https://media.giphy.com/media/xT0BKuHQMmDIoZYiCk/giphy.gif',
@@ -12,14 +23,13 @@ imageArrays.exports = {
 	'https://media.giphy.com/media/H3ADkJa2NtqYE/giphy.gif',
 	'https://media.giphy.com/media/UyLyMBQzUw1i/giphy.gif',
 	'https://s3.amazonaws.com/uploads.hipchat.com/28219/3351195/9NC2DgolFAD8p9O/upload.png'
-	]
+]
 
-	var kevinGifArray = [
+var kevinGifArray = [
 	'https://s3.amazonaws.com/uploads.hipchat.com/28219/3345171/TJ3xwtYcR1kAXz1/kevin_dancing.gif',
 	'https://s3.amazonaws.com/uploads.hipchat.com/28219/3345171/qJz4hHhQLaGNCLD/kevin_outOfTouch.gif'
-	]
+]
 
-	var kevinPictureArray = [
+var kevinPictureArray = [
 	'https://s3.amazonaws.com/uploads.hipchat.com/28219/3345171/xEKmLRnD4PBAluX/kevin_finger.png'
-	]
-};
+]
